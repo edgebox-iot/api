360e5547a376727f3d0c5b4b7c887d2cda9d5f81
Paulo Truta <pinheirotruta5@gmail.com>
2021-02-15 17:55:16 +0100

    Added better handling of post/get cases and current task status for setup/access view

--- src/app/Controllers/App.php
+++ src/app/Controllers/App.php
@@ -42,22 +42,31 @@ class App extends Controller {
         $status = 'Waiting for Edgebox.io Account Credentials';
         $connection_status = 'Not connected';
         $connection_details = [];
+        $task_status = 0;
 
-        if(!empty($this->f3->get('POST.username')) && !empty($this->f3->get('POST.password'))) {
+        $is_post_request = !empty($this->f3->get('POST.username')) && !empty($this->f3->get('POST.password'));
+
+        if($is_post_request) {
+
+            // POST Request. Try to login to edgebox.io, obtain jwt token, save necessary info as options, issue setup of tunnel.
+
             // User submitted login credentials for API.
             $api_token = $edgeboxio_api->get_token($this->f3->get('POST.username'), $this->f3->get('POST.password'));
             if($api_token['status'] == 'success') {
                 
-                // Save API token in database, for future requests.
+                // Successfully got an jwt token. Save the token in database, for future requests.
                 $options->load(array('name=?','EDGEBOXIO_API_TOKEN'));
                 $options->name = 'EDGEBOXIO_API_TOKEN';
                 $options->value = $api_token['value'];
+
                 $options->save();
 
                 // Request Edgebox.io API information about the bootnode.
                 $tunnel_info = $edgeboxio_api->get_bootnode_info();
 
                 if($tunnel_info['status'] == 'success') {
+
+                    // The reponse was successful. Save fetched information in options and issue setup_tunnel task.
 
                     $options->load(array('name=?','BOOTNODE_ADDRESS'));
                     $options->name = 'BOOTNODE_ADDRESS';
@@ -85,10 +94,12 @@ class App extends Controller {
                     $tasks->args = json_encode($tunnel_info['value']);
                     $tasks->save();
 
-                    $connection_status = "Configuring tunnel network...";
+                    $connection_status = "Configuring tunnel network for " . $tunnel_info['value']['node_name'] . "...";
                     $connection_details = $tunnel_info['value'];
 
                 } else {
+
+                    // An error in the /bootnode API endpoint occured. Display error to user.
 
                     $status = json_encode($tunnel_info['value']);
                 
@@ -97,20 +108,69 @@ class App extends Controller {
                 
             } else {
 
+                // An error occured with the login process (bad credentials, service unavailable, etc.) Display error to user.
+
                 $status = $api_token['value'];
             
             }
-        }
 
-        $options = new Options();
-        $options->load(array('name=?','EDGEBOXIO_API_TOKEN'));  
-        $show_form  = true;
-        $api_token = '';
+        } else {
 
-        if(!empty($options->value)) {
-            $status = "Logged in to Edgebox.io";
-            $show_form = false;
-            $api_token = $options->value;
+            // GET Request. Should get latest setup_tunnel task status and display it.
+
+            $options->load(array('name=?','EDGEBOXIO_API_TOKEN'));
+            $api_token = $options->value;  
+            $show_form  = true;
+
+            if(!empty($api_token)) {
+            
+                // We have an API token, which means that a previous login and tunnel setup was made.
+                // We can check the task status.
+
+                $show_form = false;
+
+                // Is already logged in, and not doing this request through post
+
+                $tunnel_info = $edgeboxio_api->get_bootnode_info($api_token);
+                $connection_details = $tunnel_info['value'];
+
+                $status = "Logged in to Edgebox.io as " . $connection_details['node_name'];
+                $tunnel_setup_task = new Tasks();
+                $tunnel_setup_task->load(array('task=?','setup_tunnel'));
+                $task_status = $tunnel_setup_task->status;
+                print_r($task_status);
+                switch($task_status) {
+                    case 0:
+
+                        print_r("GOTCHA");
+                        // Task has not yet been picked up by edgeboxctl...
+                        $connection_status = "Waiting for Edgebox to start executing the setup...";
+                        break;
+
+                    case 1:
+
+                        // Task has been picked up by edgeboxctl and is not in progress...
+                        $connection_status = "Configuring tunnel network for " . $connection_details['node_name'] . "...";
+                        // TODO: Some sort of auto-reload when the status is this one could be very useful.
+                        break;
+
+                    case 2:
+
+                        // Task is complete and has result. In this, case the apps we will allow registration in the myedge.app service.
+                        $connection_status = 'Successfully connected to myedge.app Service';
+                        
+                        break;
+
+                    default:
+
+                        // Error occurred and should be shown to the user.
+                        $connection_status = json_decode($tunnel_setup_task->result)['value'];
+
+
+                }
+
+            
+            }
 
         }
 
@@ -121,6 +181,7 @@ class App extends Controller {
                 'status' => $status,
                 'connection_status' => $connection_status,
                 'connection_details' => $connection_details,
+                'task_status' => $task_status,
                 'api_token' => $api_token,
             ]
         );
