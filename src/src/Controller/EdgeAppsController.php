8c6ff6f6b41d37708144d3b1042749e915b0c27a
Paulo Truta <pinheirotruta5@gmail.com>
2021-05-01 14:28:53 +0000

    Added EdgeApps controller base code and twig template

--- src/src/Controller/EdgeAppsController.php
+++ src/src/Controller/EdgeAppsController.php
@@ -2,22 +2,63 @@
 
 namespace App\Controller;
 
+use App\Entity\Option;
+use App\Repository\OptionRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EdgeAppsController extends AbstractController
 {
+
+    /**
+     * @var OptionRepository
+     */
+    private $optionRepository;
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(
+        OptionRepository $optionRepository,
+        EntityManagerInterface $entityManager
+    )
+    {
+        $this->optionRepository = $optionRepository;
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/edgeapps", name="edgeapps")
      */
     public function index(): Response
     {
+
+        $framework_ready = false;
+        $apps_list = [];
+        $tunnel_on = false;
+
+        $apps_list_option = $this->optionRepository->findOneBy(['name' => 'EDGEAPPS_LIST']) ?? new Option();
+        $apps_list = $apps_list_option->getValue();
+
+        if(!empty($apps_list)) {
+            $apps_list = json_decode($apps_list, true);
+            $tunnel_on_option = $this->optionRepository->findOneBy(['name' => 'BOOTNODE_TOKEN']) ?? new Option();
+            $tunnel_on = !empty($tunnel_on_option->getValue());
+            $framework_ready = true;
+        }
+
         // TODO: Port EdgeApps control logic from src-f3
-        return $this->render('not_available.html.twig', [
+        return $this->render('edgeapps/index.html.twig', [
             'controller_name' => 'EdgeAppsController',
             'controller_title' => 'EdgeApps',
             'controller_subtitle' => 'Applications control',
+            'framework_ready' => $framework_ready,
+            'apps_list' => $apps_list,
+            'tunnel_on' => $tunnel_on
         ]);
     }
 }
