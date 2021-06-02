c7ca47db26fa0e037cdeed5eadf9471417bd7067
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-02 21:39:36 +0200

    Fix: Actions overview support for disable_tunnel and setup_tunnel actions, handle unknown actions (#20)
    * Actions overview support for disable_tunnel and setup_tunnel actions
    
    * Fixes to action ENABLE_ONLINE, backported url fetching method, pre-prepare task args
    
    * PHPStan + PHPCS Fix
    
    * Limited max number of activity to show to 5, handling unknown action case gracefully
    
    * Fix bug, wrong key looking for action icon

--- src/src/Helper/EdgeAppsHelper.php
+++ src/src/Helper/EdgeAppsHelper.php
@@ -45,4 +45,29 @@ class EdgeAppsHelper
 
         return $found;
     }
+
+    public function getInternetUrl(string $app_id): ?string
+    {
+        $url = null;
+
+        $token_option = $this->optionRepository->findOneBy(['name' => 'EDGEBOXIO_API_TOKEN']) ?? new Option();
+
+        if (null === $token_option->getValue()) {
+            return $url;
+        }
+
+        $edgeboxio_api = new EdgeboxioApiConnector();
+        $url_registration_response = $edgeboxio_api->register_apps($token_option->getValue(), $app_id);
+
+        if (!empty($url_registration_response['status']) && 'success' == $url_registration_response['status']) {
+            $app_info = !empty($url_registration_response['value']['apps'][$app_id]) ? $url_registration_response['value']['apps'][$app_id] : [];
+
+            // Check if registration was successfull and only then issue the appliance to set configurations.
+            if (!empty($app_info) && !empty($app_info['url'])) {
+                $url = $app_info['url'];
+            }
+        }
+
+        return $url;
+    }
 }
