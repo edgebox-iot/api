d9d0cdc962f8e693ad5d8ad1f9e2bf6cd1e1ce26
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-04 12:44:28 +0000

    Using mocks to test (failing)

--- src/tests/Unit/Task/TaskFactoryTest.php
+++ src/tests/Unit/Task/TaskFactoryTest.php
@@ -3,14 +3,29 @@
 namespace App\Tests\Unit\Task;
 
 use App\Factory\TaskFactory;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 class TaskFactoryTest extends TestCase
 {
-    public function testCreateDisableTunnelTask(): void
+
+    public function testCreateEnableOnlineTask(): void
     {
-        $task = TaskFactory::createDisableTunnelTask();
-        self::assertEquals('disable_tunnel', $task->getTask());
-        self::assertEquals('[]', $task->getArgs());
+        $factory_mock = $this->getTaskFactoryMock();
+        $task = $factory_mock->createEnableOnlineTask('test');
+
+        self::assertEquals('enable_online', $task->getTask());
+        self::assertEquals(json_encode(['id' => 'test', 'internet_url' => null]), $task->getArgs());
+        self::assertEquals(3, $task->getStatus());
+    }
+
+    private function getTaskFactoryMock(): MockObject
+    {
+        $optionRepositoryMock = $this->getMockBuilder(\App\Repository\OptionRepository::class)->disableOriginalConstructor()->getMock();
+        $optionRepositoryMock->method('findOneBy')->will($this->returnValue(null));
+
+        $edgeAppsHelperMock = $this->getMockBuilder(\App\Helper\EdgeAppsHelper::class)->disableOriginalConstructor()->getMock();
+
+        return $this->getMockBuilder(\App\Factory\TaskFactory::class)->setConstructorArgs([$optionRepositoryMock, $edgeAppsHelperMock])->getMock();
     }
 }
