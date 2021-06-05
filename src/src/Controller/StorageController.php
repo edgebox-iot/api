f02f021561b044c500d2c2d76a2120ff9a0ec706
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-05 22:27:51 +0000

    Added StorageHelper class, setup base variables for readiness, added getStorageDevices method

--- src/src/Controller/StorageController.php
+++ src/src/Controller/StorageController.php
@@ -6,23 +6,42 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Helper\StorageHelper;
+
 class StorageController extends AbstractController
 {
+
+    public function __construct(
+        StorageHelper $storageHelper
+    ) {
+        $this->storageHelper = $storageHelper;
+    }
+
     /**
-     * @Route("/storage", name="storage")
-     */
+    * @Route("/storage", name="storage")
+    */
     public function index(): Response
     {
+
+        $storage_ready = false;
+        $storage_devices_list = $this->storageHelper->getStorageDevicesList();
+
+        if(!empty($storage_devices_list)) {
+            $storage_ready = true;
+        }
+
         return $this->render('storage/index.html.twig', [
             'controller_name' => 'StorageController',
             'controller_title' => 'Storage',
             'controller_subtitle' => 'Buckets & Drives',
+            'storage_ready' => $storage_ready,
+            'storage_devices' => $storage_devices_list
         ]);
     }
 
-     /**
-     * @Route("/storage/device/new", name="storage_device_new")
-     */
+    /**
+    * @Route("/storage/device/new", name="storage_device_new")
+    */
     public function storage_device_new(): Response
     {
         return $this->render('storage/device/new.html.twig', [
---
+++ src/src/Helper/StorageHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Helper;
+
+use App\Entity\Option;
+use App\Repository\OptionRepository;
+
+class StorageHelper
+{
+    private OptionRepository $optionRepository;
+
+    public function __construct(
+        OptionRepository $optionRepository
+    ) {
+        $this->optionRepository = $optionRepository;
+    }
+
+    public function getStorageDevicesList(): array
+    {
+        $storage_devices_list_option = $this->optionRepository->findOneBy(['name' => 'STORAGE_DEVICES_LIST']) ?? new Option();
+
+        if (null === $storage_devices_list_option->getValue()) {
+            return [];
+        }
+
+        return json_decode($storage_devices_list_option->getValue(), true);
+    }
+}
