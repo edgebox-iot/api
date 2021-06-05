9e63c88ec8134837ba9207f94f3d8cc0c7c48239
Paulo Truta <pinheirotruta5@gmail.com>
2021-06-05 13:17:13 +0000

    Added test for storage_device_new route

--- src/tests/Functional/Controller/StorageControllerTest.php
+++ src/tests/Functional/Controller/StorageControllerTest.php
@@ -6,10 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class StorageControllerTest extends WebTestCase
 {
-    public function testHomeAction(): void
+    public function testHomeRoute(): void
     {
         $client = static::createClient();
         $client->request('GET', '/storage');
+        self::assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testAddNewDeviceRoute(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/storage/device/new');
         self::assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
