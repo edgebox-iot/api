95bfc91bd0059a0e673c9c8826ce59d4f82d5b19
Malachi Soord <inverse.chi@gmail.com>
2021-06-09 21:59:33 +0200

    Model more

--- src/tests/Unit/Helper/EdgeAppsHelperTest.php
+++ src/tests/Unit/Helper/EdgeAppsHelperTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Unit\Helper;
 
+use App\Entity\Option;
 use App\Helper\EdgeAppsHelper;
 use App\Helper\EdgeboxioApiConnector;
 use App\Repository\OptionRepository;
@@ -18,5 +19,34 @@ class EdgeAppsHelperTest extends TestCase
         $edgeAppsHelper = new EdgeAppsHelper($mockOptionRepository, $mockEdgeboxioApiConnector);
 
         self::assertEquals([], $edgeAppsHelper->getEdgeAppsList());
+    }
+
+    public function testGetEdgeAppsListOption(): void
+    {
+        $option = new Option();
+        $option->setValue(
+            json_encode(
+                [
+                    [
+                        'id' => 1,
+                        'status' => [
+                            'description' => 'on',
+                        ],
+                        'description' => 'Something awesome',
+                        'name' => 'Something',
+                        'internet_accessible' => true,
+                        'network_url' => 'https://example.com',
+                    ],
+                ],
+                JSON_THROW_ON_ERROR
+            )
+        );
+        $mockOptionRepository = $this->getMockBuilder(OptionRepository::class)->disableOriginalConstructor()->getMock();
+        $mockOptionRepository->method('findOneBy')->willReturn($option);
+
+        $mockEdgeboxioApiConnector = $this->getMockBuilder(EdgeboxioApiConnector::class)->disableOriginalConstructor()->getMock();
+        $edgeAppsHelper = new EdgeAppsHelper($mockOptionRepository, $mockEdgeboxioApiConnector);
+
+        self::assertCount(1, $edgeAppsHelper->getEdgeAppsList());
     }
 }
