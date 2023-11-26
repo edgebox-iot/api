fc640ad11420b1a213c92a1483df327e9740ed38
Malachi Soord <inverse.chi@gmail.com>
2023-11-26 22:03:55 +0100

    fix lint

--- src/src/Repository/UserRepository.php
+++ src/src/Repository/UserRepository.php
@@ -6,8 +6,8 @@ use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
-use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
